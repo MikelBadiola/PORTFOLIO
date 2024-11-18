6230145a9eeeb6e45338a95dbd5d294f3c7ad28d
MikelBadiola <mbadiolagon@alumni.unav.es>
2024-11-18 19:56:33 +0100

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Mikel Badiola",
+  title: "Eco + Data analytics",
+  email: "mikelbad05@gmail.com",
+  gitHub: "MikelBadiola",
   linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
